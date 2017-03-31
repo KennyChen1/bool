961108baab53f75eb4f5e9e6e2166ee8a589d284
Michael Dadurian <mdadurian@gmail.com>
2017-03-31 16:47:31 -0400

    started evaluator.js

--- script/evaluator.js
+++ script/evaluator.js
@@ -17,11 +17,20 @@ function evaluate(){
 /* sees if a grid or island(or any list) has a circuit generating component*/
 function hasSignalGeneratoringComponent(list){
 
+    for (var i = 0; i < list.length; i++){
+        if (list[i].type == NOT_GATE_COMPONENT || list[i].type == ON_BOX_COMPONENT){
+            return true;
+        }
+    }
+
+    return false;
 }
 
 /* find all islands of a circuit on the grid */
 function findAllIslands(grid){
-
+    for (var i = 0; i < grid.length; i++){
+        
+    }
 }
 
 /* find the beginning of an island of circuits*/
@@ -31,4 +40,5 @@ function findBeginnings(island){
 
 console.log("Hello World");
 console.log(grid);
+
 
