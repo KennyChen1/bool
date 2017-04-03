8652543d5f2ff7f8b2f70de8a75a4ac07e70567f
Kenny <kenny.chen@stonybrook.edu>
2017-04-03 00:00:14 -0400

    finished finding islands for the most part

--- script/evaluator.js
+++ script/evaluator.js
@@ -39,76 +39,113 @@ function findAllIslands(grid){
 	island = [];
 	seen = [];
 	gridcopy = grid.slice(0) // make a copy
-    while (gridcopy.length > 0){
-   		centerComp = gridcopy.pop();
-   		centerCompLoc = centerComp.locations()   		
-   		if(island.length == 0){ // base case when the no compontents in the island
-   			seen.push(centerComp)
-   			island.push([centerComp]);
-   		}
+	count = 0
+	while (gridcopy.length > 0){
+
+		centerComp = gridcopy.pop();
+		centerCompLoc = centerComp.locations() 
+		
+		found = false;
+		if(island.length == 0){ 						// base case when the no compontents in the island
+			island.push([centerComp]);
+			seen.push(centerComp)
+		} else {
+   			for(v = 0; v < island.length; v++){			// get the length of how many island
+				for(w = 0; w < island[v].length; w++){	// get the length of how many components per island
+					if(centerComp === island[v][w]){	// go through the entire thing to check if this comp is checked
+						found = true
+						break;
+					}
+				}		
+			}
+			if(found == false){
+				console.log("new island2")
+				island.push([centerComp]);
+				seen.push(centerComp)
+			}			
+		}
+		
+
+		// this sections gets the bounds to get scan for adjacentcy
+   		jlower = centerCompLoc[0].x-1
+		klower = centerCompLoc[0].y-1
+    	if(centerComp.width == 2){    		
+	    	if(centerComp.direction == 1 ||centerComp.direction == 2){   			// right side up or upside down
+	    		jupper = centerCompLoc[1].x+1
+	    		kupper = centerCompLoc[0].y+1
+	    	} else{
+	    		//sideways/vertical
+	    		jupper = centerCompLoc[0].x+1
+	    		kupper = centerCompLoc[1].y+1
+	    	}
+	    } else if(centerComp.width == 1){    		
+	    		jupper = centerCompLoc[0].x+1
+	    		kupper = centerCompLoc[0].y+1
+	    }
+
+	    count  = 0;		// counter to check for diagonalls because they are not adjacent
+		for(j =jlower; j <= jupper; j++){		// x direction to check
+			for(k = klower; k <= kupper; k++){	// y direction to check
+				count++
 
 
-    	// if widith is 1 or 2 not checked
-    	// doesn't work with 1 x 1 and rotated 1 x 2s
-    	// so only works in default oritentation or upside down
-    	if(centerComp.direction == 1 ||centerComp.direction == 2){   			// right side up or upside down   		
+				if((centerComp.width == 2) && (count == 1 || count == 3 || count == 10 || count == 12 || count == 5 || count == 8)){
+					// things not to check in a 2x1
+					// 1 3 10 12 are the corners, those are not adjacent
+					// 5 and 8 are the centre components
+					continue;
+					console.log("this should never print")
+				} else if((centerComp.width == 1) && (count == 1 || count == 3 || count == 7 || count == 9 || count == 5)){
+					// things not to check in a 1x1
+					// 1 3 10 12 are the corners, those are not adjacent
+					// 5 is the centre components
+					continue;
+					console.log("this should never print")
+				} else{
+					//?????
+				}	
+				
+				
+				if(getAtGrid(j, k))	{ 		// something is found
+					seenbool = false;		// variable/boolean to check if the found piece is not already been seen/added
 
+					// should check if this isn't comp isn't visted yet
+					for(i = 0; i < seen.length; i++){
+						if(seen[i] === getAtGrid(j, k)){									
+							seenbool = true;
+							break;
+						}
+					}
 
-    		for(j = centerCompLoc[0].x-1; j <= centerCompLoc[1].x+1; j++){		// x direction to check
-    			for(k = centerCompLoc[0].y-1; k <= centerCompLoc[0].y+1; k++){	// y direction to check
+					// should be that when nothing in seen has been found, it is not part of any island
+					if(seenbool == false){
+						found = false		// boolean/variable to keep track if the center has been found yet in the island
 
-    				// is not the center piece to check from
-    				if(!(centerCompLoc[0].x == j && centerCompLoc[0].y == k) && !(centerCompLoc[1].x == j && centerCompLoc[1].y == k)){ 
-    					if(getAtGrid(j, k))	{ 		// something is found
-    						
-    						seenbool = false;		// variable/boolean to check if the found piece is not already been seen/added
-
-    						// should check if this isn't comp isn't visted yet
-    						for(i = 0; i < seen.length; i++){
-    							if(seen[i] === getAtGrid(j, k)){									
-									seenbool = true;
-									break;
-    							}
-    						}
-
-    						// should be that when nothing in seen has been found, it is not part of any island
-    						if(seenbool == false){
-    							found = false		// boolean/variable to keep track if the center has been found yet in the island
-
-    							// this foor loop is used to determine which island to add too
-    							for(v = 0; v < island.length; v++){			// get the length of how many island
-    								for(w = 0; w < island[v].length; w++){	// get the length of how many components per island
-										if(centerComp === island[v][w]){
-											island[v].push(getAtGrid(j, k));
-											found = true
-											break;
-										}
-									}
+						// this foor loop is used to determine which island to add too
+						for(v = 0; v < island.length; v++){			// get the length of how many island
+							for(w = 0; w < island[v].length; w++){	// get the length of how many components per island
+								if(centerComp === island[v][w]){
+									island[v].push(getAtGrid(j, k));
+									found = true										
 								}
-    								
-							 	if(found == false)
-							 		island.push([getAtGrid(j, k)])			// appends a new island to the list of island							 	
-	    						seen.push(getAtGrid(j, k))					// adds to the seen list to not double add
-							}	// end of seen bool == false
-						}	// end of if(getAtGrid(j, k))
-    				} else{
-    					// does condition when there is nothing at this coordinate
-    					//console.log(j + " " + k) // the center component
-    				}
-    				
-    			}
-    		}
-    	} else{	// the other orientation
-    		console.log("x: " + (centerComp[0].x-1) + " " + (centerComp[0].x+1))
-    		console.log("y: " + (centerComp[0].y-1) + " " + (centerComp[1].y+1))
+							}
+						}
 
-    	}	
+						if(found == false){	// nothing has been found to be adj so is a new island
+					 		island.push([getAtGrid(j, k)])			// appends a new island to the list of island
+					 	}
+					 	seen.push(getAtGrid(j, k))					// adds to the seen list to not double add
+					}	else{
+						continue;
+					}// end of if(seenbool == false)
 
-
-    	
-        
-    }
+				}	// end of if(getAtGrid(j, k))				
+			}	// end of for(k = klower; k <= kupper; k++)
+		}	// end of for(j =jlower; j <= jupper; j++)
+	} // end of the while loop at the top
 }
+
+
 
 /* find the beginning of an island of circuits*/
 function findBeginnings(island){
