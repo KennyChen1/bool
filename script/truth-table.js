62658b0f402d61c3b02852877adae2ffd05f6fa7
Nelson <nelnel65@hotmail.com>
2017-04-28 19:08:50 -0400

    scrapped table layout, just uses textbox, each newline is seperated by a /

--- script/truth-table.js
+++ script/truth-table.js
@@ -1,3 +1,15 @@
+function getTruthTableAsString(){
+	var ttString = $(".console #truth-table #alt-textbox #truth-tb").val();
+	console.log(ttString);
+	return ttString;
+}
+
+function getTruthTableAsArray(){
+
+}
+
+/* Useless functions, may be useful in the future
+
 var ttSelected; //keeps track of which box in the table u currently have selected(coords)
 var rowNum = 1000; 
 var colLabel = "B";
@@ -7,9 +19,15 @@ function addColumnToTruthTable(){
 	var allRows = $(".console #truth-table #input .tt-row");
 	headerRow.append("<th>"+colLabel+"</th>");
 	allRows.append("<td>0</td>");
+
+	var numCols = Math.pow(2, $(".console #truth-table #input th").length - 1) - allRows.length;
+
+	for (var i = numCols - 1; i >= 0; i--) {
+		addRowToTruthTable(i);
+	}
 }
 
-function addRowToTruthTable(){
+function addRowToTruthTable(rowNum){
 	var theTable = $(".console #truth-table #input .table-input");
 	var rowToAdd = $("<tr>", {"class": "tt-row"});
 	var rowSize = $(".console #truth-table #input .header th").length;
@@ -26,6 +44,29 @@ function addRowToTruthTable(){
 	theTable.append(rowToAdd);
 }
 
+function deleteColumnFromTruthTable(){
+	var headerRow = $(".console #truth-table #input .header");
+	var allRows = $(".console #truth-table #input .tt-row");
+	if(headerRow.find("th").length > 1){
+		var headerRowTh = headerRow.find("th:last");
+		var allRowsTh = allRows.find("td:last");
+
+		for (var i = (allRowsTh.length)/2; i >= 1; i--) {
+			deleteRowFromTruthTable();
+		}
+
+		headerRowTh.remove();
+		allRowsTh.remove();
+	}
+}
+
+function deleteRowFromTruthTable(){
+	var rows = $(".console #truth-table #input .table-input");
+
+	rows.find("tr:last").remove();
+}
+
+
 function convertTextBoxToTruthTable(){
 
 }
@@ -37,7 +78,4 @@ function convertTruthTableToTextBox(){
 function toggleTruthTableTextBox(){
 
 }
-
-function getTruthTableAsArray(){
-
-}
+*/
